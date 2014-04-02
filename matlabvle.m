c4a88d54e9439df976b7402dd202cedc7992adeb
Patrice Lecharpentier <patrice.lecharpentier@paca.inra.fr>
2014-04-02 11:18:22 +0200

    Changes: Class help properties attributes new method : addDynMethods modified: existMethodName calling Vle instance method.

--- matlabvle.m
+++ matlabvle.m
@@ -1,70 +1,146 @@
 
 classdef (ConstructOnLoad) matlabvle < dynamicprops
+    %
+    % matlabvle Properties:
+    %  objectHandle - Handle to the underlying C++ class instance
+    %  method_code - Template for creating anonymous function handle
+    %  methods_names - Cell of method names to be added as dynamic properties
+    %
+    % matlabvle Methods:
+    %  callMethod - static, for executing any C++ class method through added dynamic
+    %  properties
+    %  listMethods - display methods list (added dynamically from
+    %  methods_names
+    %  matlabvle - Class constructor
+    %  delete - Class destructor
+    %  getHandle - getting the object handle
+    %  addDynMethods - Adding dynamic properties with an anonymous function
+    %  handle as value
+    %  addDynProp - adding a single dynamic property
+    %  getMethodHandle - formatting an anonymous function for a specific
+    %  method name
+    %  getMethodsNames - Getting active methods list
+    %  existMethodName - Evaluating if a method exists in C++ Vle class
+    
     properties (SetAccess = private, Hidden = true)
-        % Handle to the underlying C++ class instance
+        % handle to class instance
         objectHandle;
-        % Template of anonymous function handle
-        method_code='@(varargin) matlabvle.call(uint64(object_handle),''name'',varargin{:})';
+        % anonymous function template
+        method_code='@(varargin) matlabvle.callMethod(uint64(object_handle),''name'',varargin{:})';
+    end
+    
+    properties (Constant=true, Hidden=true)
         % C++ methods names to be added dynamically as properties
-        % containing a handle to a dedicated anonymous function
+        % each containing a handle to a dedicated anonymous function
+        % executing a method of the instanciated C++ Vle class of 
+        % matlab_vle_mex.cpp
+        %
         methods_names={'getFileName' 'setFileName' 'getPackageName' 'setPackageName'...
             'openFile' 'save' 'getStatus' 'getHomeDir' ...
             'getExperimentName' 'getBegin' 'setBegin' 'getDuration' 'setDuration' ...
-            'setOutputPlugin' ...
+            'setOutputPlugin' 'getOutputPlugin' ...
             'addCondition' 'delCondition' 'getConditionsSize' 'listConditions' 'getConditionPortsSize'...
             'listConditionPorts' 'clearConditionPort' 'getConditionPortValuesSize'...
             'getConditionPortValues' 'getConditionPortValue' 'getConditionValueType' ...
             'addRealCondition' 'addIntegerCondition' 'addStringCondition' 'addBooleanCondition'...
-            'getViewOutput' 'getViewsSize' 'listViews' 'existView' ...
+            'getViewOutput' 'getViewsSize' 'listViews' 'existView' 'addView' ...
             'getDynamicsSize' 'listDynamics' 'getDynamicModelsSize' 'listDynamicModels' 'setConditionValue' ...
             'existObservable' 'listObservables' 'getObservablesSize' 'clearObservables' ...
             'addObservable' 'delObservable' ...
             'listObservablePorts' 'getObservablePortsSize' 'addObservablePort' 'delObservablePort' ...
             'existObservablePortView' 'addViewToObservablePort' 'delViewFromObservablePort' ...
-            'run'};
+            'getAtomicModelId' 'getModelObservables' 'setModelObservables'... 
+            'getOutputPortNumber' 'existOutputPort' 'addOutputPort' 'delOutputPort' 'listOutputPorts'...
+            'run' ...
+            'existMethod'};
     end
     
     methods (Static)
-        % Calling C++ matlabvle_interface_mex methods
-        function varargout=call(handle_id,name,varargin)
+        function varargout=callMethod(handle_id,name,varargin)
+            %
+            % callMethod(handle_id,name,varargin)
+            %
+            %   Calling C++ matlabvle_interface_mex methods
+            %      handle_id - object handle
+            %      name - method name
+            %      varargin : method input arguments
+            
             [varargout{1:nargout}] = matlabvle_interface_mex(name, handle_id, varargin{:});
         end
         
+        
+        function listMethods()
+            %
+            % listMethods()
+            % 
+            %   Diplaying dynamically added methods list
+            %
+            list=matlabvle.methods_names;
+            sprintf('%s\n',list{:})
+        end
     end
     
     methods     
         function this = matlabvle(varargin)
-            % Constructor - Create a new C++ Vle class instance
+            %
+            % matlabvle()
+            %
+            %   Constructor - Create a new C++ Vle class instance
+            
             % Handle to C++ instance
             this.objectHandle = matlabvle_interface_mex('new', varargin{:});
-            % Adding dynamic properties, each containing a handle to a dedicated
-            % anonymous function calling a c++ method
-            for i=1:length(this.methods_names)
-                method_name=this.methods_names{i};
-                if this.existMethodName(method_name)
-                    this.addDynProp(method_name,this.getMethodHandle(method_name));
-                else
-                    fprintf('%s : unreferenced method name !',method_name); 
-                    disp('Use obj.getMethodsNames to get complete list of interfaced methods!');
-                end
-            end
-        end
-            
-        function delete(this)
-            % Destructor - Destroy the C++ Vle class instance
-            matlabvle_interface_mex('delete', this.objectHandle);
+            % Adding C++ Vle class methods as dynamic properties
+            this=addDynMethods(this);
         end
         
+            
+        function delete(this)
+            %
+            % delete()
+            %
+            %    Destructor - Destroy the C++ Vle class instance
+            matlabvle_interface_mex('delete', this.getHandle());
+        end
+        
+        
         function obj_handle=getHandle(this)
+            %
+            % obj_handle=getHandle()
+            %
+            %   Getting handle to object
             obj_handle=this.objectHandle;
         end
          
-        %%
+        
+        function this=addDynMethods(this)
+            %
+            % addDynMethods()
+            %
+            %   Adding dynamic properties, each containing a handle to a dedicated
+            %   anonymous function calling a c++ method
+            %   from methods_names property content
+            
+            exist_methods=this.existMethodName(this.methods_names);
+            for i=1:length(this.methods_names)
+                method_name=this.methods_names{i};
+                if exist_methods(i)
+                    this.addDynProp(method_name,this.getMethodHandle(method_name));
+                else
+                    fprintf('\n\n%s : unreferenced method name !\n',method_name); 
+                    disp('Check C++ file matlabvle_interface_mex.cpp (in setMethodsList method) !');
+                end
+            end
+        end
+        
+        
         function addDynProp(this,prop_name,prop_value)
-            % Adding a dynamic property and set its value
-            % to a matlabvle instance.
-            % prop_name: property name
-            % prop_value : property value
+            %
+            % addDynProp(property_name,property_value)
+            %
+            %   Adding a dynamic property and set its value
+            %   to a matlabvle instance.
+            %      prop_name - property name
+            %      prop_value - property value
             if ~isprop(this,prop_name)
                 new_prop = addprop(this,prop_name);
                 new_prop.Transient=true;
@@ -76,48 +152,63 @@ classdef (ConstructOnLoad) matlabvle < dynamicprops
             end
         end
         
+        
         function method_handle = getMethodHandle(this,method_name)
-            % Formatting method handle from method_code property
-            % and method name
-            % method_name : name of a declared method of underlying C++
+            %
+            % getMethodHandle(method_name)
+            %
+            %   Formatting method handle from method_code property
+            %   and method name
+            %
+            % method_name - name of a declared method of underlying C++
             % class in methods_names property
             % method_handle : handle to a specific anonymous function
+            
             if this.existMethodName(method_name)
                 % adding checking if method name exists
-                new_method_str=strrep(this.method_code,'object_handle',num2str(this.objectHandle));
+                new_method_str=strrep(this.method_code,'object_handle',num2str(this.getHandle()));
                 method_handle=str2func(strrep( new_method_str,'name',method_name));
             else
                 method_handle=[];
             end
         end
         
+        
         function methods_list = getMethodsNames(this)
-            % Getting declared method names list
-            % from methods_name property
-            methods_list = this.methods_names;
+            % 
+            % getMethodsNames()
+            %
+            %   Getting declared method names list
+            %   from methods_name property
+            
+            exist_methods=this.existMethodName(this.methods_names);
+            methods_list = this.methods_names(exist_methods);
         end
+        
         
         function method_exists = existMethodName(this,method_name)
-            % Evaluates if a method name is declared in class
-            % method_name : name of a method
-            % method_exists : true if exists, false instead
-           method_exists=ismember(method_name,this.methods_names);
-        end
-        
-        function help(this,method_name)
-           %this.getMethodHelp(method_name);
-           this.addDynProp('methodHelp',[method_name ' help content']);
-           disp(this.methodHelp);
-        end
-        
-        function set_methodHelp(this,val)
-            if  ~(ischar(val))
-                error('methodHelp requires a char input ')
+            %
+            % method_exists = existMethodName(method_name)
+            %
+            %   Evaluates if a method name is declared in class
+            %
+            %      method_name - name of a method
+            %      method_exists - true if exists, false instead
+            
+            if ischar(method_name)
+                method_name={method_name};
             end
-            this.methodHelp = val; % set property value
+            method_exists=logical(cellfun(@(x) matlabvle_interface_mex('existMethod', this.getHandle(),x), method_name));
         end
-
-
         
+        
+%         function set_methodHelp(this,val)
+%             if  ~(ischar(val))
+%                 error('methodHelp requires a char input ')
+%             end
+%             this.methodHelp = val; % set property value
+%         end
+
+
     end
 end
